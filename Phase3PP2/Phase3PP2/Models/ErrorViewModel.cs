1973d1aef709f009156748b508944cbaedb81b0e
Silambarasan <msilambarasan98@gmail.com>
2023-09-22 09:43:52 +0530

    first commit

---
+++ Phase3PP2/Phase3PP2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Phase3PP2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
